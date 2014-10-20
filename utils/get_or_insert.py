2c9b2b2056434e893955c0b2b29662f10f2d011e
Florian Kaiser <florian.kaiser@fnkr.net>
2014-10-20 11:38:09 +0200

    shorten str to the max len if column has a max len, fix #16

--- utils/get_or_insert.py
+++ utils/get_or_insert.py
@@ -1,6 +1,9 @@
 from app import db
 
 def get_or_insert(table, column, value, **kwargs):
+    if type(value) == str and getattr(column.type, 'length'):
+        value = value[:column.type.length]
+
     entry = table.query.filter(column == value).first()
     if entry:
         return entry
