a692296549aba2e313adf7484f36e9d93650f248
Florian Kaiser <florian.kaiser@fnkr.net>
2014-10-28 10:21:39 +0100

    do not try to make graph if there is no data (fix "Uncaught TypeError: Cannot read property 'getContext' of undefined")

--- app/static/js/stats/overview.js
+++ app/static/js/stats/overview.js
@@ -1,12 +1,14 @@
-var ctx = $("#chart").get(0).getContext("2d");
+if (window.data !== undefined) {
+    var ctx = $("#chart").get(0).getContext("2d");
 
-if (data.labels.length == 1) {
-    data.labels.unshift('');
-    for (i = 0; i < data.datasets.length; i++) {
-        try {
-            data.datasets[i].data.unshift(0);
-        } catch(e) {
+    if (data.labels.length == 1) {
+        data.labels.unshift('');
+        for (i = 0; i < data.datasets.length; i++) {
+            try {
+                data.datasets[i].data.unshift(0);
+            } catch(e) {
+            }
         }
     }
+    var chart = new Chart(ctx).Line(data);
 }
-var chart = new Chart(ctx).Line(data);
