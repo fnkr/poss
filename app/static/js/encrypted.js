ee56d9c2db2e8340384cab9df32fd02f1310f380
Florian Kaiser <florian.kaiser@fnkr.net>
2020-12-11 15:58:31 +0100

    Generate random key

--- app/static/js/encrypted.js
+++ app/static/js/encrypted.js
@@ -1,1 +1,7 @@
-$('#encrypted_content').text(CryptoJS.AES.decrypt($('#encrypted_content').text(), "My Secret Passphrase").toString(CryptoJS.enc.Utf8));
+$(document).ready(function() {
+    if (window.location.hash === '') {
+        window.location.hash = localStorage.getItem('poss.key');
+        localStorage.setItem('poss.key', '');
+    }
+    $('#encrypted_content').text(CryptoJS.AES.decrypt($('#encrypted_content').text(), window.location.hash.substring(1)).toString(CryptoJS.enc.Utf8));
+});
